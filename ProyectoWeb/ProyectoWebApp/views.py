23c437a75e8c1b3539e21d24e8bf25b1de895a46
pablotebb <4356464+pablotebb@users.noreply.github.com>
2025-05-01 17:21:06 +0100

    Video 28

--- ProyectoWeb/ProyectoWebApp/views.py
+++ ProyectoWeb/ProyectoWebApp/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-  return HttpResponse("Home")
+  return render(request, "ProyectoWebApp/home.html")
 
 def servicios(request):
-  return HttpResponse("Servicios")
+  return render(request, "ProyectoWebApp/servicios.html")
 
 def tienda(request):
-  return HttpResponse("Tienda")
+  return render(request, "ProyectoWebApp/tienda.html")
 
 def blog(request):
-  return HttpResponse("Blog")
+  return render(request, "ProyectoWebApp/blog.html")
 
 def contacto(request):
-  return HttpResponse("Contacto")
+  return render(request, "ProyectoWebApp/contacto.html")
