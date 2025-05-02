3a13ff6d2f241759009e59c75e88e19cb0639965
pablotebb <4356464+pablotebb@users.noreply.github.com>
2025-05-02 18:25:07 +0100

    Video 41

--- ProyectoWeb/Blog/urls.py
+++ ProyectoWeb/Blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.blog, name="Blog"),
+    path("categoria/<int:categoria_id>/", views.categoria, name="categoria"),
 ]
 
--- ProyectoWeb/Blog/views.py
+++ ProyectoWeb/Blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Categoria
 
 # Create your views here.
 def blog(request):
   posts = Post.objects.all()
   return render(request, "Blog/blog.html", {"posts": posts})
+
+def categoria(request, categoria_id):
+  categoria = Categoria.objects.get(id=categoria_id)
+  posts = Post.objects.filter(categorias=categoria)
+  return render(request, "Blog/categoria.html", {"categoria": categoria, "posts": posts})
