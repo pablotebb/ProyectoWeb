2f0efe3214ff7ee6aadc4e7a5ef697f335032032
pablotebb <4356464+pablotebb@users.noreply.github.com>
2025-05-03 16:49:07 +0100

    Video 49

--- ProyectoWeb/Tienda/views.py
+++ ProyectoWeb/Tienda/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from .models import Producto
 
 # Create your views here.
 
 def tienda(request):
-  return render(request, "Tienda/tienda.html")
+  productos = Producto.objects.all()
+  return render(request, "Tienda/tienda.html", {"productos": productos})
 
 
