e367387918cc2203402b5e8298c4b6ba1ad21e27
pablotebb <4356464+pablotebb@users.noreply.github.com>
2025-05-04 21:25:00 +0100

    Video 66

--- ProyectoWeb/Carro/context_processor.py
+++ ProyectoWeb/Carro/context_processor.py
@@ -3,8 +3,10 @@
 def importe_total_carro(request):
   # carro = Carro(request)
   total = 0
-  # if request.user.is_authenticated:
-  '''for key, value in request.session["carro"].items():
-    total = total + float(value["precio"])'''
+  if request.user.is_authenticated:
+    for key, value in request.session["carro"].items():
+      total = total + float(value["precio"])
+  else:
+    total = "Debes hacer login"
     
   return {"importe_total_carro": total}
--- ProyectoWeb/ProyectoWebApp/views.py
+++ ProyectoWeb/ProyectoWebApp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from Carro.carro import Carro
 
 
 # Create your views here.
 def home(request):
+  carro = Carro(request)
   return render(request, "ProyectoWebApp/home.html")
 
 
