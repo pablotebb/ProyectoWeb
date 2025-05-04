e7601eaacf18a7df0e4f984483665b212ff103b9
pablotebb <4356464+pablotebb@users.noreply.github.com>
2025-05-04 17:34:47 +0100

    Video 62

--- ProyectoWeb/Autenticacion/views.py
+++ ProyectoWeb/Autenticacion/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
+from django.contrib.auth import login
+from django.contrib import messages
 
 from django.contrib.auth.forms import UserCreationForm
 
@@ -12,4 +14,13 @@ class VRegistro(View):
     return render(request, "registro/registro.html", {"form": form})
   
   def post(self, request):
-    pass
+    form = UserCreationForm(request.POST)
+    
+    if form.is_valid():
+      usuario = form.save()
+      login(request, usuario)
+      return redirect("Home")
+    else:
+      for msg in form.error_messages:
+        messages.error(request, form.error_messages[msg])
+      return render(request, "registro/registro.html", {"form": form})
--- ProyectoWeb/Carro/context_processor.py
+++ ProyectoWeb/Carro/context_processor.py
@@ -4,7 +4,7 @@ def importe_total_carro(request):
   # carro = Carro(request)
   total = 0
   # if request.user.is_authenticated:
-  for key, value in request.session["carro"].items():
-    total = total + float(value["precio"])
+  '''for key, value in request.session["carro"].items():
+    total = total + float(value["precio"])'''
     
   return {"importe_total_carro": total}
