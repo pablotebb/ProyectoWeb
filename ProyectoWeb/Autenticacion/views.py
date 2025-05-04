b82664d0ac47c5ffd2b0d1b0aea8c0f0f3fcf787
pablotebb <4356464+pablotebb@users.noreply.github.com>
2025-05-04 19:07:17 +0100

    Video 63

--- ProyectoWeb/Autenticacion/views.py
+++ ProyectoWeb/Autenticacion/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib import messages
 
 from django.contrib.auth.forms import UserCreationForm
@@ -24,3 +24,7 @@ class VRegistro(View):
       for msg in form.error_messages:
         messages.error(request, form.error_messages[msg])
       return render(request, "registro/registro.html", {"form": form})
+
+def cerrar_sesion(request):
+  logout(request)
+  return redirect("Home")
