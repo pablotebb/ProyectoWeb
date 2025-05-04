3ead7fa044dda5338ceb93d4d370fcb29cd22540
pablotebb <4356464+pablotebb@users.noreply.github.com>
2025-05-04 13:31:33 +0100

    Video 60

--- ProyectoWeb/Autenticacion/urls.py
+++ ProyectoWeb/Autenticacion/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import VRegistro
 
 urlpatterns = [
-    path("", views.autenticacion, name="Autenticacion"),
+    path("", VRegistro.as_view(), name="Autenticacion"),
 ]
 
